16b6a48513bbc2bf30e2154822c8a3cfde2124fd
godfredakpan <godfredakpan@gmail.com>
2024-02-29 16:24:41 +0100

    update changes

--- app/apartmentDetails/page.tsx
+++ app/apartmentDetails/page.tsx
@@ -154,18 +154,13 @@ export default function CourseDetails() {
                       <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
                       </svg>
-                      1 bedroom - 2 guests, 2 bedrooms - 4 guests, 3 bedrooms - 6 guests, 4 bedrooms - 8 guests e.tc "Each room has 2 guests Max"
+                      1 bedroom - 2 guests, 2 bedrooms - 4 guests, 3 bedrooms - 6 guests, 4 bedrooms - 8 guests e.tc - Each room has 2 guests Max
                     </li>
 
                   </ul>
                 </div>
 
-                {/* <button
-                      style={{ backgroundColor: '#191F59' }}
-                      className="bg-green-500 text-size-20 text-sm text-white px-2 py-1 rounded mr-5 text-right"
-                    >
-                      Book Apartment
-                  </button> */}
+
               </div>
 
             </div>
